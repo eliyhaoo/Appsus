f12c8b74126b06586ce3a0a02f13debd6d23830e
eliyhaoo <102289745+eliyhaoo@users.noreply.github.com>
2022-05-01 23:43:05 +0300

    one more afte rthis

--- js/cmps/app-header.jsx
+++ js/cmps/app-header.jsx
@@ -2,22 +2,42 @@
 const {Link, NavLink} = ReactRouterDOM
 
 
-export function AppHeader() {
+export class AppHeader extends React.Component {
 
+       state ={
+            isMenuOpen: false,
+            app: ""
+       }
+
+       toggleMenu=()=>{
+           console.log('closing menu',this.state);
+        this.setState((prevState)=>({...prevState,isMenuOpen:!this.state.isMenuOpen}))
+       }
+
+       setCurrApp=(appName)=>{
+        this.setState((prevState)=>({...prevState,app:appName}))
+       }
+
+       render(){
+
+        const {isMenuOpen,app} = this.state
+        const menuClass = !isMenuOpen ? '':'open-menu'
 
         return <header className="app-header flex space-between flex-center">
         <div className="logo-container flex">
-        <Link to='/'><h3 className="logo">App<span>sus</span></h3> </Link>
-        {/* <h3 className="selected-app">Mail</h3> */}
+        <Link to='/'><h3 onClick={()=>this.setCurrApp('')} className="logo">App<span>sus</span></h3> </Link>
         </div>
+        <h3>{app}</h3>
 
-        <label className="menu-container" htmlFor="chk"> <img src="assets/img/icons/apps-icon.png" alt="menu" /></label>
-        <input type="checkbox" id="chk" />
+    
 
-            <nav className="nav-bar">
+        <div onClick={this.toggleMenu} className="menu-container" htmlFor="chk"> <img src="assets/img/icons/apps-icon.png" alt="menu" /></div>
+    
 
-                <NavLink to='/keep'>
-                    <div className="keep-link">
+            <nav  onClick={this.toggleMenu}  className={`nav-bar ${menuClass}`}>
+
+                <NavLink  to='/keep'>
+                    <div onClick={()=>this.setCurrApp('Keep')} className="keep-link">
                         <div className="keep-img-container">
                             <img src="assets/img/icons/keep.png" alt="keep" />
                         </div>
@@ -27,7 +47,7 @@ export function AppHeader() {
 
         <label htmlFor="chk">
                 <NavLink to='/mail'>
-                    <div className="mail-link">
+                    <div onClick={()=>this.setCurrApp('Gmail')} className="mail-link">
                         <div className="mail-img-container">
                             <img src="assets/img/icons/gmail.png" alt="mail" />
                         </div>
@@ -38,5 +58,5 @@ export function AppHeader() {
             </nav>
     </header>
 
-
+}
 }
